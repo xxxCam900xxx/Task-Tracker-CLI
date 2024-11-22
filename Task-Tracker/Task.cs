76923341e7c2c81a204a69f6f4a3a88e69d40fcd
Developer <112639963+xxxCam900xxx@users.noreply.github.com>
2024-11-22 15:44:22 +0100

    🚧 chore: Working Enviroment

---
+++ Task-Tracker/Task.cs
@@ -0,0 +1,9 @@
+﻿namespace Task_Tracker
+{
+    public class Task
+    {
+        public int TaskID { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+    }
+}
---
+++ Task-Tracker/TaskTracker.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Management.Automation;
+
+namespace Task_Tracker
+{
+
+    [Cmdlet(VerbsCommon.Get, "NITPerf")]
+    [OutputType(typeof(Task))]
+    public class TaskTracker : Cmdlet
+    {
+
+        private string myparameter;
+        private string myparameter2;
+        private string myparameter3;
+
+        [Parameter]
+        public string Myparameter { get => myparameter; set => myparameter = value; }
+
+        [Parameter(ValueFromPipelineByPropertyName = true, ValueFromPipeline = true)]
+        public string Myparameter2 { get => myparameter2; set => myparameter2 = value; }
+
+        [Parameter]
+        public string Myparameter3 { get => myparameter3; set => myparameter3 = value; }
+        protected override void ProcessRecord()
+        {
+
+            /*
+            Console.WriteLine("Hello World3");
+            var re = new MyReturn();
+            re.Name = "A1";
+            re.Description = "Beschreibung";
+            */
+
+            Random ro = new Random();
+
+            List<Task> returnList = new List<Task>();
+            for (int i = 0; i < 10; i++)
+            {
+                var r1 = new Task();
+                r1.Name = ro.Next().ToString();
+                r1.Description = ro.Next().ToString();
+                WriteObject(r1);
+                // returnList.Add(r1);
+            }
+
+            // ReadOnlyCollection<MyReturn> re = new ReadOnlyCollection<MyReturn>(returnList);
+            //WriteObject(returnList);
+        }
+    }
+}
