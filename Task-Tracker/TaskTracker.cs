e6e7555010d26bb3c186430f89f35c44ac07a0d6
Developer <112639963+xxxCam900xxx@users.noreply.github.com>
2024-11-28 16:47:52 +0100

    🚧 chore: Removed Comments and added Nice Info

--- Task-Tracker/TaskTracker.cs
+++ Task-Tracker/TaskTracker.cs
@@ -5,47 +5,21 @@ using System.Management.Automation;
 namespace Task_Tracker
 {
 
-    [Cmdlet(VerbsCommon.Get, "NITPerf")]
+    [Cmdlet(VerbsCommon.Get, "AllTask")]
     [OutputType(typeof(Task))]
-    public class TaskTracker : Cmdlet
+    public class GetAllTasks : Cmdlet
     {
-
-        private string myparameter;
-        private string myparameter2;
-        private string myparameter3;
-
-        [Parameter]
-        public string Myparameter { get => myparameter; set => myparameter = value; }
-
-        [Parameter(ValueFromPipelineByPropertyName = true, ValueFromPipeline = true)]
-        public string Myparameter2 { get => myparameter2; set => myparameter2 = value; }
-
-        [Parameter]
-        public string Myparameter3 { get => myparameter3; set => myparameter3 = value; }
+        // This is the WriteMethod to Write in Powershell!
         protected override void ProcessRecord()
         {
-
-            /*
-            Console.WriteLine("Hello World3");
-            var re = new MyReturn();
-            re.Name = "A1";
-            re.Description = "Beschreibung";
-            */
-
-            Random ro = new Random();
-
-            List<Task> returnList = new List<Task>();
-            for (int i = 0; i < 10; i++)
-            {
-                var r1 = new Task();
-                r1.Name = ro.Next().ToString();
-                r1.Description = ro.Next().ToString();
-                WriteObject(r1);
-                // returnList.Add(r1);
-            }
-
-            // ReadOnlyCollection<MyReturn> re = new ReadOnlyCollection<MyReturn>(returnList);
-            //WriteObject(returnList);
+            WriteObject("Hello World");
         }
+    }
+
+    [Cmdlet(VerbsCommon.Add, "NewTask")]
+    [OutputType(typeof(Task))]
+    public class CreateNewTask : Cmdlet
+    {
+
     }
 }
